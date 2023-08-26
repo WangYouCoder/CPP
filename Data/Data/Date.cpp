78c421909689ca88def7fe45b3af5bee1a8a0a5c
WangYouCoder <123453572+WangYouCoder@users.noreply.github.com>
2023-08-26 22:05:59 +0800

    日期类

--- Data/Data/Date.cpp
+++ Data/Data/Date.cpp
@@ -1,3 +1,225 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include"Date.h"
 
+int Date::GetMonthDay(int year, int month)
+{
+	int _month[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	if ((_year % 4 == 0 && _year % 100 != 0) || _year % 400 == 0)
+	{
+		_month[2] = 29;
+	}
+
+	return _month[month];
+}
+
+Date::Date(int year,  int month, int day)
+	:_year(year)
+	, _month(month)
+	, _day(day)
+{
+	if (_month < 1 || _month > 12 ||
+		_day < 1 || _day > GetMonthDay(_year, _month))
+	{
+		cout << "日期错误" << endl;
+	}
+}
+
+Date::Date(const Date& d)
+{
+	_year = d._year;
+	_month = d._month;
+	_day = d._day;
+}
+
+Date::~Date()
+{
+	_year = _month = _day = 0;
+}
+
+
+Date& Date::operator=(const Date& d)
+{
+	_year = d._year;
+	_month = d._month;
+	_day = d._day;
+
+	return *this;
+}
+
+Date& Date::operator+=(int day)
+{
+	if (day < 0)
+	{
+		return *this -= day;
+	}
+	_day += day;
+	if (_day > GetMonthDay(_year, _month))
+	{
+		_day -= GetMonthDay(_year, _month);
+		_month++;
+		if (_month == 13)
+		{
+			_month = 1;
+			_year++;
+		}
+	}
+	return *this;
+}
+
+Date Date::operator+(int day)
+{
+	Date tmp(*this);
+	tmp += day;
+	return tmp;
+}
+
+Date Date::operator-(int day)
+{
+	Date tmp(*this);
+	tmp -= day;
+	return tmp;
+}
+
+Date& Date::operator-=(int day)
+{
+	if (day < 0)
+	{
+		return *this += (-_day);
+	}
+	_day -= day;
+	while (_day <= 0)
+	{
+		_month--;
+		if (_month == 0)
+		{
+			_month = 12;
+			_year--;
+		}
+		_day += GetMonthDay(_year, _month);
+	}
+	return *this;
+}
+
+Date& Date::operator++()
+{
+	*this += 1;
+	return *this;
+}
+
+Date Date::operator++(int)
+{
+	Date tmp(*this);
+	*this += 1;
+	return tmp;
+}
+
+Date Date::operator--(int)
+{
+	Date tmp(*this);
+	*this -= 1;
+	return tmp;
+}
+
+Date& Date::operator--()
+{
+	*this -= 1;
+	return *this;
+}
+
+bool Date::operator>(const Date& d)
+{
+	if (_year < d._year)
+	{
+		return false;
+	}
+	else if (_year == d._year && _month < d._month)
+	{
+		return false;
+	}
+	else if (_month == d._month && _day < d._day)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool Date::operator==(const Date& d)
+{
+	return _year == d._year
+		&& _month == d._month
+		&& _day == d._day;
+}
+
+bool Date::operator >= (const Date& d)
+{
+	return (*this == d) && (*this > d);
+}
+
+bool Date::operator < (const Date& d)
+{
+	return !(*this >= d);
+}
+
+bool Date::operator <= (const Date& d)
+{
+	return !(*this > d);
+}
+
+bool Date::operator != (const Date& d)
+{
+	return !(*this == d);
+}
+
+int Date::operator-(const Date& d)
+{
+	//int day = 0;
+	//day = _day - d._day;
+	//while (_month >= d._month)
+	//{
+	//	_month--;
+	//	if (_month > 0)
+	//	{
+	//		day += GetMonthDay(_year, _month);
+	//	}
+	//}
+
+	//while (_year > d._year)
+	//{
+	//	_year--;
+	//	if ((_year % 4 == 0 && _year % 100 != 0) || _year % 400 == 0)
+	//	{
+	//		day += 366;
+	//	}
+	//	else
+	//	{
+	//		day += 355;
+	//	}
+	//}
+	//return day;
+
+	Date max = *this;
+	Date min = d;
+	int flag = 1;
+
+	if (max < min)
+	{
+		max = d;
+		min = *this;
+		flag = -1;
+	}
+
+	int n = 0;
+	while (min < max)
+	{
+		min++;
+		n++;
+	}
+
+	return n * flag;
+}
+
+void Date::Print()
+{
+	cout << _year << "年" << _month << "月" << _day << endl;
+}
--- Data/Data/test.cpp
+++ Data/Data/test.cpp
@@ -1,8 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include"Date.h"
 
+//void test1()
+//{
+//	Date d1(2023, 8, 26);
+//	Date d2 = d1;
+//	d2.Print();
+//
+//	Date d3(2023, 8, 31);
+//	Date ret1 = d3++;
+//	ret1.Print();
+//	d3.Print();
+//
+//	/*int day = d3 - d1;
+//	cout << day << endl;*/
+//}
+
+void test2()
+{
+	Date d1(2023, 8, 26);
+	Date d2(2023, 8, 27);
+
+	cout << d1 - d2 << endl;
+}
+
 int main()
 {
-
+	//test1();
+	test2();
 	return 0;
 }
